31c72b787869600b77a383d4ad24023c6d0da9a7
tumul-unisim <mailtotumulkumarsinha@gmail.com>
2026-03-17 18:56:54 +0530

    modified:   src/navigation/index.js

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -2,9 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 
 import MainFlow from "./MainFlow";
 
-export const Navigation = createBrowserRouter([
-  MainFlow,
-]);
+export const Navigation = createBrowserRouter([MainFlow], {
+  basename: "/syntra-dashboard-demo",
+});
 
 export default Navigation;
-
